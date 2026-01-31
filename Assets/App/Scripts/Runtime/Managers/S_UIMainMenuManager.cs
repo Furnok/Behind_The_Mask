231396c4125e09b2b8d1165300e675d1dbe33e16
Killian FURNO <killian.furno@orange.fr>
2026-01-31 10:08:04 +0100

    Fix: Window Manager

--- Assets/App/Scripts/Runtime/Managers/S_UIMainMenuManager.cs
+++ Assets/App/Scripts/Runtime/Managers/S_UIMainMenuManager.cs
@@ -25,8 +25,20 @@ public class S_UIMainMenuManager : MonoBehaviour
     [TabGroup("Inputs")]
     [SerializeField] private RSE_OnMainMenu rseOnMainMenu;
 
+    [TabGroup("Inputs")]
+    [SerializeField] private RSE_OnOpenWindow rseOnOpenWindow;
+
+    [TabGroup("Inputs")]
+    [SerializeField] private RSE_OnCloseWindow rseOnCloseWindow;
+
+    [TabGroup("Inputs")]
+    [SerializeField] private RSE_OnCloseAllWindows rseOnCloseAllWindows;
+
     [TabGroup("Outputs")]
     [SerializeField] private RSE_OnShowMouseCursor rseOnShowMouseCursor;
+
+    [TabGroup("Outputs")]
+    [SerializeField] private RSO_CurrentWindows rsoCurrentWindows;
 
     [TabGroup("Outputs")]
     [SerializeField] private SSO_FadeTime ssoFadeTime;
@@ -40,11 +52,17 @@ public class S_UIMainMenuManager : MonoBehaviour
     private void OnEnable()
     {
         rseOnMainMenu.action += Setup;
+        rseOnOpenWindow.action += AlreadyOpen;
+        rseOnCloseWindow.action += CloseWindow;
+        rseOnCloseAllWindows.action += CloseAllWindows;
     }
 
     private void OnDisable()
     {
         rseOnMainMenu.action -= Setup;
+        rseOnOpenWindow.action -= AlreadyOpen;
+        rseOnCloseWindow.action -= CloseWindow;
+        rseOnCloseAllWindows.action -= CloseAllWindows;
     }
 
     private void Setup()
@@ -68,6 +86,59 @@ public class S_UIMainMenuManager : MonoBehaviour
             }));
         }));
 
+    }
+
+    private void AlreadyOpen(GameObject window)
+    {
+        if (window != null)
+        {
+            if (!window.activeInHierarchy) OpenWindow(window);
+            else CloseWindow(window);
+        }
+    }
+
+    private void OpenWindow(GameObject window)
+    {
+        CanvasGroup cg = window.GetComponent<CanvasGroup>();
+        cg.DOKill();
+
+        window.SetActive(true);
+
+        cg.DOFade(1f, ssoDisplayWindowTime.Value.time).SetEase(Ease.Linear).SetUpdate(true);
+
+        rsoCurrentWindows.Value.Add(window);
+    }
+
+    private void CloseWindow(GameObject window)
+    {
+        if (window != null && window.activeInHierarchy)
+        {
+            CanvasGroup cg = window.GetComponent<CanvasGroup>();
+            cg.DOKill();
+
+            cg.DOFade(0f, ssoDisplayWindowTime.Value.time).SetEase(Ease.Linear).SetUpdate(true).OnComplete(() =>
+            {
+                window.SetActive(false);
+            });
+
+            rsoCurrentWindows.Value.Remove(window);
+        }
+    }
+
+    private void CloseAllWindows()
+    {
+        foreach (var window in rsoCurrentWindows.Value)
+        {
+            CanvasGroup cg = window.GetComponent<CanvasGroup>();
+            cg.DOKill();
+
+            cg.DOFade(0f, ssoDisplayWindowTime.Value.time).SetEase(Ease.Linear).SetUpdate(true).OnComplete(() =>
+            {
+                window.SetActive(false);
+            });
+        }
+
+        rsoCurrentWindows.Value.Clear();
     }
 
     private void FadeIn()
