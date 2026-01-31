a5ea4ca522b9c66f812d424ca748e0368b50b09d
N3crom <mathias.manenq@gmail.com>
2026-01-31 14:31:41 +0100

    feat: door mask interactible have no animation just dissapear now

--- Assets/App/Scripts/Runtime/Gameplay/Interaction/Interactible/S_DoorInteractible.cs
+++ Assets/App/Scripts/Runtime/Gameplay/Interaction/Interactible/S_DoorInteractible.cs
@@ -5,12 +5,15 @@ public class S_DoorInteractible : MonoBehaviour, IInteractable
     [Header("Settings")]
     [SerializeField] int _priority = 0;
     [SerializeField] bool _isInteractable = true;
+    [SerializeField] Mask _requiredMask;
 
-    //[Header("References")]
+    [Header("References")]
+    [SerializeField] GameObject _doorVisuals;
 
     //[Header("Inputs")]
 
-    //[Header("Outputs")]
+    [Header("Outputs")]
+    [SerializeField] RSO_PlayerCurrentMaskEquipped _playerCurrentMaskEquipped;
 
     public int Priority => _priority;
     public Transform Transform => transform;
@@ -18,6 +21,11 @@ public class S_DoorInteractible : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-
+        if(_playerCurrentMaskEquipped.Value == _requiredMask)
+        {
+            // Logic to open the door and animation
+            Debug.Log("Door opened with mask: " + _requiredMask.ToString());
+            _doorVisuals.SetActive(false); // Simulate door opening by disabling visuals for now
+        }
     }
 }
